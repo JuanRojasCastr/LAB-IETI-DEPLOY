8155554ae3ee562339b70a77824fe6ea3c328a6d
Yesid MORA <yesid.mora@mail.escuelaing.edu.co>
2023-03-01 21:51:09 -0500

    first commit

---
+++ src/main/java/escuelaing/edu/co/name/NameApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameApplication.class, args);
+	}
+
+}
---
+++ src/main/java/escuelaing/edu/co/name/controller/UserController.java
@@ -0,0 +1,17 @@
+package escuelaing.edu.co.name.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/name")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> getName() {
+        return ResponseEntity.ok("My name is YESID CAMILO MORA BARBOSA");
+    }
+
+}
